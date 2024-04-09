084696b0921ce3201eaaad24f15e7a5c892f578a
17diego15 <a24957@svalero.com>
2024-04-09 19:58:35 +0200

    Tabla codigo penal actualizada

--- Data/Migrations/20240409175507_InitialCreate.cs
+++ Data/Migrations/20240409175507_InitialCreate.cs
@@ -121,7 +121,37 @@ namespace RuleStreet.Data.Migrations
                 {
                     { 1, "Art. 1.1", "Uso excesivo del claxón", 500m, "0 meses" },
                     { 2, "Art. 1.2", "Giro indebido", 300m, "0 meses" },
-                    { 3, "Art. 1.3", "Circular en sentido contrario", 700m, "0 meses" }
+                    { 3, "Art. 1.3", "Circular en sentido contrario", 700m, "0 meses" },
+                    { 4, "Art. 1.4", "Estacionar en zonas no habitadas y obstruir la circulación", 500m, "0 meses" },
+                    { 5, "Art. 1.5", "Ignorar señales de tránsito", 500m, "0 meses" },
+                    { 6, "Art. 1.6", "Saltarse un semáforo", 450m, "0 meses" },
+                    { 7, "Art. 1.7", "No ceder el paso a vehículos de emergencia", 550m, "0 meses" },
+                    { 8, "Art. 1.8", "Realizar adelantamiento indebido", 350m, "0 meses" },
+                    { 9, "Art. 1.9", "Circular marcha atrás", 800m, "0 meses" },
+                    { 10, "Art. 1.10", "Ignorar señales de los agentes que regulan la circulacion", 500m, "0 meses" },
+                    { 11, "Art. 1.11", "Saltarse / omitir un control de trafico", 500m, "0 meses" },
+                    { 12, "Art. 1.12", "Conducir un vehiculo en malas condiciones", 1000m, "0 meses" },
+                    { 13, "Art. 1.13", "Exceso de velocidad en vias urbanas", 800m, "5 meses" },
+                    { 14, "Art. 1.14", "Condución temeraria", 1500m, "10 meses" },
+                    { 15, "Art. 1.15", "Exceso de velocidad en vias secundarias", 1000m, "10 meses" },
+                    { 16, "Art. 1.16", "Conducir bajo los efectos de drogas/alcohol", 1500m, "10 meses" },
+                    { 17, "Art. 1.17", "Circular por zonas no habilitadas para ello", 1000m, "0 meses" },
+                    { 18, "Art. 1.18", "Circular sin casco para motocicleta", 500m, "0 meses" },
+                    { 19, "Art. 1.18.1", "Circular sin licencia de conducir", 550m, "0 meses" },
+                    { 20, "Art. 2.1", "Alteración del orden público", 3500m, "0 meses" },
+                    { 21, "Art. 2.2", "Racismo", 5000m, "0 meses" },
+                    { 22, "Art. 2.3", "Faltas de respeto a otro civil", 2500m, "0 meses" },
+                    { 23, "Art. 2.4", "Dañar mobiliario urbano", 1700m, "0 meses" },
+                    { 24, "Art. 2.5", "Acoso psicológico", 6000m, "5 meses" },
+                    { 25, "Art. 2.6", "Amenaza de muerte a un civil", 2000m, "5 meses" },
+                    { 26, "Art. 2.7", "Suplantación de identidad", 6000m, "0 meses" },
+                    { 27, "Art. 2.8", "Circular por la via pública con el rostro oculto", 1000m, "5 meses" },
+                    { 28, "Art. 2.9", "Circular en la via pública desnudo o semi-desnudo", 1200m, "0 meses" },
+                    { 29, "Art. 2.10", "Circular en la via pública sin camiseta", 120m, "0 meses" },
+                    { 30, "Art. 2.11", "Acoso sexual", 30000m, "100 meses" },
+                    { 31, "Art. 2.12", "Violar una orden de alejamiento con sentencia firme", 25000m, "80 meses" },
+                    { 32, "Art. 2.13", "Negativa a identificarse", 1000m, "7 meses" },
+                    { 33, "Art. 2.14", "Obstrucción a la justicia", 2500m, "10 meses" }
                 });
 
             migrationBuilder.CreateIndex(
