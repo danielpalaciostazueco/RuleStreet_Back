0c864724fab5ca3382d639b0f1b203429b84dbc3
danielpalaciostazueco <palaciostazuecodaniel@gmail.com>
2024-06-08 16:58:25 +0200

    Cambios migraciones

--- Data/Migrations/20240608141909_InitialCreate.Designer.cs
+++ Data/Migrations/20240608141909_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using RuleStreet.Data;
 namespace RuleStreet.Data.Migrations
 {
     [DbContext(typeof(RuleStreetAppContext))]
-    [Migration("20240607151603_InitialCreate")]
+    [Migration("20240608141909_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
